e0c41f5cfa2bd3d36ef09aa633158b267e8834c2
Chad Parker <chad@chadparker.com>
2020-05-19 11:22:02 -0700

    Got it!

--- bubbleSort.playground/Contents.swift
+++ bubbleSort.playground/Contents.swift
@@ -1,22 +1,44 @@
 import UIKit
 
-func bubbleSort(){
+func bubbleSort<Element: Comparable>(_ list: inout [Element], by operation: (Element, Element) -> Bool = (<)) {
     
+    var changed = false
+    
+    repeat {
+        changed = false
+        print(list)
+        for i in 0..<list.count {
+            guard i < list.count-1 else { break }
+            
+            let current = list[i]
+            let next = list[i+1]
+            
+            if operation(next, current) {
+                
+                list[i] = next
+                list[i+1] = current
+
+                changed = true
+            }
+        }
+    } while changed == true
 }
 
 
 // Test Cases
-//var arr1 = [1, 3, 6, 2, 4, 5]
-//var arr3 = [6, 5, 4, 3, 2, 1]
-//bubbleSort(&arr1) // returns [1, 2, 3, 4, 5, 6]
-//bubbleSort(&arr1, by: >) // returns [6, 5, 4, 3, 2, 1]
-//bubbleSort(&arr3) // returns [1, 2, 3, 4, 5, 6]
+var arr1 = [1, 3, 6, 2, 4, 5]
+var arr3 = [6, 5, 4, 3, 2, 1]
+
+bubbleSort(&arr1) // returns [1, 2, 3, 4, 5, 6]
+bubbleSort(&arr1, by: >) // returns [6, 5, 4, 3, 2, 1]
+bubbleSort(&arr3) // returns [1, 2, 3, 4, 5, 6]
 
 
 // Test Cases (Stretch Goal)
-//var arr4 = [1, 3, 6, 2, 4, 5]
-//var arr5 = ["b", "a", "c", "f", "e", "d", "i", "g", "h"]
-//var arr6 = [6, 5, 4, 3, 2, 1]
-//bubbleSort(&arr4) // returns [1, 2, 3, 4, 5, 6]
-//bubbleSort(&arr5, by: >) // returns ["i", "h", "g", "f", "e", "d", "c", "b", "a"]
-//bubbleSort(&arr6) // returns [1, 2, 3, 4, 5, 6]
+var arr4 = [1, 3, 6, 2, 4, 5]
+var arr5 = ["b", "a", "c", "f", "e", "d", "i", "g", "h"]
+var arr6 = [6, 5, 4, 3, 2, 1]
+bubbleSort(&arr4) // returns [1, 2, 3, 4, 5, 6]
+bubbleSort(&arr5) // returns ["i", "h", "g", "f", "e", "d", "c", "b", "a"]
+bubbleSort(&arr5, by: >) // returns ["i", "h", "g", "f", "e", "d", "c", "b", "a"]
+bubbleSort(&arr6) // returns [1, 2, 3, 4, 5, 6]
